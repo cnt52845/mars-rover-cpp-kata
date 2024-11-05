b01ccd536c0bf482eb7fcec42f74d86e3d99d252
cnt52845 <132798980+cnt52845@users.noreply.github.com>
2024-11-05 05:14:19 +0000

    Class work

--- mars_rover.h
+++ mars_rover.h
@@ -3,6 +3,7 @@
 #include <memory>
 #include <iostream>
 #include <string>
+#include <utility>
 
 /**
  * A class template to express an equality comparison interface
@@ -36,9 +37,59 @@ operator<<(std::ostream& os, const Location& location)
 class MarsRover {
 public:
     MarsRover() = delete;
-    MarsRover(Location location) : location(std::move(location)) {}
+    MarsRover(Location location) : location(std::move(location))
+    {
+        setIncrements(location.orientation);
+    }
 
-    void move(const std::string& commands) {}
+    void setIncrements(char orientation)
+    {
+        switch (orientation) {
+        case 'N':
+            dx = 0;
+            dy = 1;
+            break;
+        case 'W':
+            dx = -1;
+            dy = 0;
+            break;
+        case 'E':
+            dx = 1;
+            dy = 0;
+            break;
+        default: // S
+            dx = 0;
+            dy = -1;
+        }
+    }
+
+    void move(const std::string& commands)
+    {
+        static const char orientations[]   = {'N', 'W', 'S', 'E'};
+        unsigned int      orientationIndex = 0;
+
+        for (const char command : commands) {
+            if (command == 'L') {
+                orientationIndex++;
+            }
+            else if (command == 'R') {
+                orientationIndex--;
+            }
+            else if (command == 'F') {
+                this->location.x += dx;
+                this->location.y += dy;
+            }
+            else if (command == 'B') {
+                this->location.x -= dx;
+                this->location.y -= dy;
+            }
+        }
+        orientationIndex &= 3;
+        location.orientation = orientations[orientationIndex];
+        setIncrements(location.orientation);
+    }
 
     Location location;
+    int      dx;
+    int      dy;
 };
